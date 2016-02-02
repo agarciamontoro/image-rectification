43c500146ff8ed16b33910c1c0c529d71e5c315a
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2016-02-02 18:14:32 +0100

    Matches, epilines and epipoles

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,54 @@
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "util.hpp"
+
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-// https://github.com/Itseez/opencv/blob/master/modules/stitching/src/autocalib.cpp
-bool choleskyDecomp(Mat A, Mat &D){
-    size_t astep = A.step;
-    double* data = A.ptr<double>();
-    int size = A.cols;
-
-    if ( Cholesky(data, astep, size, 0, 0, 0) ){
-        astep /= sizeof(data[0]);
-        for (int i = 0; i < size; ++i){
-            data[i*astep + i] = (double)(1./data[i*astep + i]);
-        }
-
-        D = A.clone();
-        D.at<double>(0,1) = D.at<double>(0,2) = D.at<double>(1,2) = 0;
-
-        D = D.t();
-
-        return true;
-    }
-
-    return false;
-}
-
 int main(){
+    Mat img_1 = imread("img/Vmort1.pgm");
+    Mat img_2 = imread("img/Vmort2.pgm");
+
+    int width = img_1.cols;
+    int height = img_1.rows;
+
     int size = 3;
 
-    double vals[3][3] = {
-        {4, 12, -16},
-        {12, 37, -43},
-        {-16, -43, 98}
+    Mat PPt = Mat::zeros(size, size, CV_64F);
+
+    PPt.at<double>(0,0) = width*width - 1;
+    PPt.at<double>(1,1) = height*height - 1;
+
+    PPt *= (width*height) / 12;
+
+    double w_1 = width - 1;
+    double h_1 = height - 1;
+
+    double values[3][3] = {
+        {w_1*w_1, w_1*h_1, 2*w_1},
+        {w_1*h_1, h_1*h_1, 2*h_1},
+        {2*w_1, 2*h_1, 4}
     };
 
-    Mat A = Mat(size, size, CV_64F, vals);
-    Mat D;
+    Mat pcpct(size, size, CV_64F, values);
 
-    cout << A << endl;
+    pcpct /= 4;
 
-    if(choleskyDecomp(A,D)){
-        cout << D << endl << D.t()*D << endl;
-    }
+    cout << PPt << endl << pcpct << endl;
+
+    Vec3d epipole1, epipole2;
+    computeAndDrawEpiLines(img_1, img_2, 150, epipole1, epipole2);
+
+    cout << epipole1 << endl << epipole2 << endl;
+
+    draw(img_1, "1");
+    draw(img_2, "2");
+
+    waitKey();
+
+    destroyAllWindows();
+
 }
